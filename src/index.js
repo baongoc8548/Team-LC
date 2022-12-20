d5a60e2688e3328e67e4d546807746072fde1e28
Loc Nguyen <dinhloc2510@gmail.com>
2022-12-20 09:47:06 +0700

    create admin

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import Home from "./pages/home";
 import Card from './components/card';
 import Product from './pages/product';
 import Login from './pages/login';
+import Admin from './pages/admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,6 +23,7 @@ root.render(
         <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
+        <Route path="admin" element={<Admin />} />
 
         {/* <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
